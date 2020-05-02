8e9d62bfae24bd97883273d9439463aa4d4ed613
gauravxdhingra <grvhingra1999@gmail.com>
2020-05-03 01:50:44 +0530

    Init

---
+++ android/app/src/main/kotlin/com/example/dribbble_design_challenge_credit_card_kashware/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.dribbble_design_challenge_credit_card_kashware
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
---
+++ lib/pages/cards_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class CardsPage extends StatefulWidget {
+  CardsPage({Key key}) : super(key: key);
+
+  @override
+  _CardsPageState createState() => _CardsPageState();
+}
+
+class _CardsPageState extends State<CardsPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text(' x '),
+    );
+  }
+}
